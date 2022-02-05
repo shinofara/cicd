5cbe9d33cb80810b7ccfe3cfd966a34b4324c6a0
shinofara <shinofara+github@gmail.com>
2022-02-05 22:52:31 +0900

    元に戻す

--- main.go
+++ main.go
@@ -2,17 +2,10 @@ package main
 
 import (
 	"fmt"
-	"io"
-
-	"golang.org/x/crypto/md4"
 )
 
 func main() {
 	fmt.Println(hoge())
-	h := md4.New()
-	data := "These pretzels are making me thirsty."
-	io.WriteString(h, data)
-	fmt.Printf("MD4 is the new MD5: %x\n", h.Sum(nil))
 }
 
 func hoge() string {
